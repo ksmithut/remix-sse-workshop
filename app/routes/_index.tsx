cd45ccf846795b0632be631b44dfccf1c887bdae
ksmithut <github@ksmithut.com>
2023-10-31 06:01:50 -0600

    Implements our first EventSource to hit our test streaming endpoint

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,34 @@
+import React from 'react'
+
+function useMessages() {
+  const [messages, setMessages] = React.useState<string[]>([])
+  React.useEffect(() => {
+    function handleMessage(event: MessageEvent<string>) {
+      const message = event.data
+      setMessages((messages) => [...messages, message])
+    }
+    // Create an EventSource to listen for server-sent events
+    const eventSource = new EventSource('/todos')
+    // Attach a listener for the 'message' event
+    eventSource.addEventListener('message', handleMessage)
+    // Return a cleanup function
+    return () => {
+      // Remove the 'message' event listener
+      eventSource.removeEventListener('message', handleMessage)
+      // Close the EventSource connection when the component unmounts
+      eventSource.close()
+    }
+  }, [])
+  return messages
+}
+
 export default function Index() {
-  return <h1 className="text-3xl font-bold">Hello World</h1>
+  const messages = useMessages()
+  return (
+    <div>
+      {messages.map((message, index) => (
+        <p key={message + index}>{message}</p>
+      ))}
+    </div>
+  )
 }
