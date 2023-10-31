a8bc0dfddbc3736efe355e631b0ce66dcf832e70
ksmithut <github@ksmithut.com>
2023-10-31 06:29:23 -0600

    Implements the rest of the functionality on the front-end

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import cx from 'clsx'
 import type { LoaderFunctionArgs, ActionFunctionArgs } from '@remix-run/node'
 import { useLoaderData, Form } from '@remix-run/react'
 import todosStore from '../todos/todos.server.ts'
-import { reducer } from '../todos/todos.reducer.ts'
+import { reducer, type Todo } from '../todos/todos.reducer.ts'
 
 export async function action({ request }: ActionFunctionArgs) {
   const body = await request.formData()
@@ -12,6 +13,24 @@ export async function action({ request }: ActionFunctionArgs) {
       if (typeof label !== 'string') break
       const id = crypto.randomUUID()
       todosStore.dispatch({ type: 'add_todo', payload: { id, label } })
+      break
+    }
+    case 'complete_todo': {
+      const id = body.get('todo_id')
+      if (typeof id !== 'string') break
+      todosStore.dispatch({ type: 'complete_todo', payload: { id } })
+      break
+    }
+    case 'uncomplete_todo': {
+      const id = body.get('todo_id')
+      if (typeof id !== 'string') break
+      todosStore.dispatch({ type: 'uncomplete_todo', payload: { id } })
+      break
+    }
+    case 'delete_todo': {
+      const id = body.get('todo_id')
+      if (typeof id !== 'string') break
+      todosStore.dispatch({ type: 'delete_todo', payload: { id } })
       break
     }
   }
@@ -46,7 +65,9 @@ export default function Index() {
       <AddTodoForm />
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.label}</li>
+          <li key={todo.id}>
+            <TodoItem todo={todo} />
+          </li>
         ))}
       </ul>
     </div>
@@ -63,5 +84,25 @@ function AddTodoForm() {
       <input type="hidden" name="action" value="add_todo" />
       <input name="label" placeholder="Add Todo..." required />
     </Form>
+  )
+}
+
+function TodoItem({ todo }: { todo: Todo }) {
+  const updateAction = todo.completed ? 'uncomplete_todo' : 'complete_todo'
+  return (
+    <div className="flex">
+      <Form method="POST">
+        <input type="hidden" name="action" value={updateAction} />
+        <input type="hidden" name="todo_id" value={todo.id} />
+        <button className={cx({ 'line-through': todo.completed })}>
+          {todo.label}
+        </button>
+      </Form>
+      <Form method="POST">
+        <input type="hidden" name="action" value="delete_todo" />
+        <input type="hidden" name="todo_id" value={todo.id} />
+        <button className="text-red-500">✘</button>
+      </Form>
+    </div>
   )
 }
